d823c53b4f07631b23cdcc3d6c05e7f0c8c9dcce
KangbeenKo <sweetrainforyou@gmail.com>
2021-09-08 19:50:34 +0900

    fix: Item & feature: WearableItem

--- 12-6. 클래스 실습예제.py
+++ 12-6. 클래스 실습예제.py
@@ -7,6 +7,9 @@
 소모형 아이템: 사용 효과, 사용하기
 (단, 버리기는 버릴 수 있는 아이템만 가능하다)
 
+조건 1. 클래스 다이어그램을 참고하여 클래스를 구현한다.
+조건 2. 클래스 다이어그램에 나와있지 않은 속성과 메서드는 자유롭게 구성한다.
+
 클래스 다이어그램
 > Item
     > name: str
@@ -30,39 +33,69 @@
 
 class Item():
     revenue = 0
-    bagWeight = 0
-    maxWeight = 1000
-
+    saleWeight = 0
+    discardWeight = 0
+    
     def __init__(self, name, price, weight, isDrop):
         self.name = name
         self.price = price
         self.weight = weight
         self.isDrop = isDrop
-    
+
     def sale(self):
         print(f'{self.name}의 가격: {self.price}')
         command = input('판매하겠습니까?\t[Y/N]: ')
         if command == 'Y':
             Item.revenue += self.price
-            Item.bagWeight -= self.weight
+            Item.saleWeight += self.weight
             print(f'{self.name} 이/가 판매되었습니다. (+{self.price}G)')
         elif command == 'N':
             print(f'{self.name}의 판매가 취소되었습니다.')
         else:
             print('잘못된 커맨드를 입력하셨습니다.')
+        
+        return Item.saleWeight
 
     def discard(self):
         if self.isDrop == True:
-            Item.bagWeight -= self.weight
-            print(f'{self.name} 을/를 버렸습니다. (현재 용량: {Item.bagWeight}/{Item.maxWeight})')
+            Item.discardWeight += self.weight
+            print(f'{self.name} 을/를 버렸습니다.')
         else:
             print('이 아이템은 버릴 수 없습니다.')
+        
+        return Item.discardWeight
 
-    def packItem(self):
-        after = Item.bagWeight + self.weight
-        if after > Item.maxWeight:
-            print('최대 용량을 초과하여 아이템을 보유할 수 없습니다.')
+class WearableItem(Item):
+    wearWeight = 0
+    maxWear = 300
+    effectList = {}
+    
+    def __init__(self, name, price, weight, isDrop, isWear, effect):
+        super().__init__(name, price, weight, isDrop)
+        self.isWear = isWear
+        self.effect = effect
+
+    def wear(self):
+        if self.isWear == True:
+            after = WearableItem.wearWeight + self.weight
+            if after > WearableItem.maxWear:
+                print('더 이상 장착할 수 없습니다.')
+            else:
+                WearableItem.wearWeight = after
+                print(f'{self.name} 을/를 장착했습니다. (추가 착용 가능 무게: {WearableItem.maxWear - WearableItem.wearWeight})')
+                WearableItem.effectList[self.name] = self.effect
         else:
-            Item.bagWeight = after
-            print(f'{self.name} 을/를 가방에 넣었습니다. (현재 용량: {Item.bagWeight}/{Item.maxWeight})')
+            print('이 아이템은 장비할 수 없습니다.')
+            
 
+    def itemOff(self):
+        if self.name in WearableItem.effectList.keys():
+            WearableItem.wearWeight -= self.weight
+            del WearableItem.effectList[self.name]
+            print(f'{self.name}의 장착을 해제하였습니다. (추가 착용 가능 무게: {WearableItem.maxWear - WearableItem.wearWeight})')
+        else:
+            print('장착하지 않은 아이템은 해제할 수 없습니다.')
+
+    def showEffect(self):
+        print(f'현재 장착한 장비의 효과: {str(WearableItem.effectList.values())}')
+    

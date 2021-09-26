cb5fc338124a77e2341b22e8bbe17ed4d8ff755b
Kangbeen Ko <sweetrainforyou@gmail.com>
2021-09-26 21:48:31 +0900

    feat(re-module.py): add 're module' methods examples

--- 15. re module.py
+++ 15. re module.py
@@ -16,3 +16,66 @@ start / 매칭된 문자열의 시작 위치 / 5
 end / 매칭된 문자열의 끝 위치 / 11
 span / 매칭된 문자열의 (시작,끝) 튜플 / (5,11)
 '''
+
+import re
+
+# 1. re module의 메서드
+str = 'love your people, love your work, love yourself'
+
+'''
+1) match
+    re.match('패턴', 찾고자 하는 문자열)
+'''
+result = re.match('love', str)
+print(result)
+# <re.Match object; span=(0, 4), match='love'>
+
+result = re.match('your', str)
+print(result)
+# None
+
+'''
+2) search
+    re.search('패턴', 찾고자 하는 문자열)
+'''
+result = re.search('love', str)
+print(result)
+# <re.Match object; span=(0, 4), match='love'>
+
+result = re.search('your', str)
+print(result)
+# <re.Match object; span=(5, 9), match='your'>
+
+'''
+3) findall
+    re.findall('패턴', 찾고자 하는 문자열)
+'''
+results = re.findall('love', str)
+print(results)
+# ['love', 'love', 'love']
+
+'''
+4) finditer
+    re.finditer('패턴', 찾고자 하는 문자열)
+'''
+results = re.finditer('love', str)
+print(results)
+# <callable_iterator object at 0x1007c0fa0>
+
+'''
+5) fullmatch
+    re.fullmatch('패턴', 찾고자 하는 문자열)
+'''
+str2 = 'Boys be ambitious'
+result1 = re.fullmatch('Boys be ambitious', str2)
+result2 = re.fullmatch('Boys be ambitious!', str2)
+result3 = re.fullmatch('.*', str2) # 정규표헌식
+
+print(result1)
+# <re.Match object; span=(0, 17), match='Boys be ambitious'>
+
+print(result2)
+# None
+
+print(result3)
+# <re.Match object; span=(0, 17), match='Boys be ambitious'>
